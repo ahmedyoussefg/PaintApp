24196aaca26816c226ee1a1689c315c861fe015b
alihassann191 <120989145+alihassann191@users.noreply.github.com>
2023-12-06 23:51:59 +0200

    Add files via upload

--- front-end/App.vue
+++ front-end/App.vue
@@ -2,6 +2,26 @@
   <div class="main">
     <div class="tools">
       <ul class="shapes">
+         <li><button @click="get_element">Select</button></li>
+         <li><button @click="Undo">Undo</button></li>
+         <li><button @click="Redo">Redo</button></li>
+         <li><button @click="Save">Save</button></li>
+        <div v-if="showModalSave" class="modal">
+        <div class="modal-content">
+        <h2>Choose File Type</h2>
+        <button >Save as XML</button>
+        <button @click="saveAsJSON">Save as JSON</button>
+        </div>
+        </div>
+         <li><button @click="Load">Load</button></li>
+         <div v-if="showModalload" class="modal">
+        <div class="modal-content">
+        <h2>Choose File Type</h2>
+        <button >Load XML File</button>
+        <button @click="saveAsJSON">Load JSON File</button>
+        </div>
+        </div>
+
         <li><button @click="drawshape('pencil')"><span>&#9998;</span> Pencil</button></li>
         <li><button @click="drawshape('line')"><span style="font-size: medium;">&#9644; </span> Line</button></li>
         <li> <button @click="drawshape('circle')"> <span>&#9679; </span> Circle</button></li>
@@ -42,7 +62,8 @@ import Triangle from './Triangle';
 import ellipse   from './ellipse';
 import Line from './Line';
 import ShapeDTO from './ShapeDTO';
-import ShapeFactory from './ShapeFactory';
+import axios from 'axios';
+import { saveAs} from 'file-saver';
 
 export default {
   data(){
@@ -56,7 +77,18 @@ export default {
       isDrawing : false,
       shapetype :'null',
       circle: null,
-      rec:null
+      rec:null,
+      sq:null,
+      line : null,
+      ellip : null,
+      pen : null,
+      triangle : null,
+      shapeID:0,
+      board :[],
+      showModalload :false,
+      showModalSave :false
+
+
     }
   },
   mounted() {
@@ -70,25 +102,242 @@ export default {
   },
 
   methods: {
-
-    requestBackend(shapeData)
+    Load()
     {
-      axios.post('http://localhost:8081/draw',shapeData)
+      this.showModalload = true
+    },
+    loadJSON()
+    {
+      // func to load JSON
+      this.showModalload = false
+    },
+
+    Save() {
+      this.showModalSave = true;
+    },
+    loadJSON(){
+      
+    },
+     saveAsJSON() {
+      this.fetchAndSaveData();
+      this.showModalSave = false;
+    },
+    async fetchAndSaveData() {
+      try {
+        const response = await axios.get('http://192.168.1.114:8081/saveJSON');
+        const jsonData = response.data;
+        const jsonString = JSON.stringify(jsonData, null, 2);
+        const blob = new Blob([jsonString], { type: 'application/json' });
+        const fileName = 'data.json';
+        saveAs(blob, fileName);
+      } catch (error) {
+        console.error('Error fetching or saving data:', error);
+      }
+    },
+    async requestsave()
+    {
+      axios.post('http://192.168.1.114:8081/saveJSON')
+      .then(response => {
+      const jsonData = JSON.stringify(response.data, null, 2);
+      const blob = new Blob([jsonData], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      }) 
+    },
+    async requestdraw(shapeData)
+    {
+      axios.post('http://192.168.1.114:8081/draw',shapeData)
       .then (response => {
+        this.shapeID=response.data.id;
         console.log('Shape saved successfully:',response.data)
       })
       .catch (error => {
         console.error('Error saving shape:' , error);
       });
-
-      
     },
+    async requestundo() {
+    return axios.post('http://192.168.1.114:8081/undo') // Returning the promise
+    .then(response => {
+      this.board = response.data;
+      return this.board; 
+    })
+    .catch(error => {
+      console.error('Error receiving shapes', error);
+      throw error; 
+    });
+    },
+    async requestredo() {
+      return axios.post('http://192.168.1.114:8081/redo') // Returning the promise
+    .then(response => {
+      this.board = response.data;
+      return this.board; 
+    })
+    .catch(error => {
+      console.error('Error receiving shapes', error);
+      throw error; 
+    });
+    },
+
+  draw(boardData) {
+  this.layer.removeChildren();
+  for (let i = 0; i < boardData.length; i++) {
+
+    switch (boardData[i].shapeType) {
+      case 'circle': {
+        this.circle = Circle.drawcircle({
+          x: boardData[i].x,
+          y: boardData[i].y,
+          radius: boardData[i].radius,
+          fill: boardData[i].fill,
+          strokeWidth: boardData[i].strokeWidth,
+          stroke: boardData[i].strokeColor,
+          draggable: false,
+        });
+       
+        this.layer.add(this.circle);
+        this.stage.batchDraw();
+        break;
+      }
+      case 'rectangle': {
+        this.rec = rectangle.drawrectangle({
+          x: boardData[i].x,
+          y: boardData[i].y,
+          width: boardData[i].width,
+          height: boardData[i].height,
+          fill: boardData[i].fill,
+          strokeWidth : boardData[i].strokeWidth,
+          stroke :boardData[i].strokeColor,
+          draggable:false,
+        });
+        console.log("Test rectangle");
+        console.log(this.rec);
+        
+        this.layer.add(this.rec); 
+        this.stage.batchDraw();
+        break;
+      }
+      case 'triangle':
+      { 
+        this.triangle = Triangle.drawTriangle({
+        x: boardData[i].x,
+        y: boardData[i].y,
+        sides: boardData[i].sides,
+        radius: boardData[i].radius,  
+        radiusX: boardData[i].radiusX,
+        side: boardData[i].side,
+        fill: boardData[i].fill,
+        strokeWidth : boardData[i].strokeWidth,
+        stroke :boardData[i].strokeColor,
+        draggable:false,
+      });
+      this.layer.add(this.triangle);
+      this.stage.batchDraw();
+        break;
+      }
+      case 'ellipse':{
+        this.ellip = ellipse.drawellipse({
+        x: boardData[i].x,
+        y: boardData[i].y,
+        radiusX: boardData[i].radiusX,
+        radiusY: boardData[i].radiusY,
+        fill: boardData[i].fill,
+        strokeWidth : boardData[i].strokeWidth,
+        stroke :boardData[i].strokeColor,
+        draggable:false,
+      });
+     
+      this.layer.add(this.ellip);
+      this.stage.batchDraw();
+        break;
+      }
+
+      case 'line_segment':{
+        this.line = Line.drawLine({
+        points: boardData[i].points,
+        stroke: boardData[i].strokeColor,
+        strokeWidth: boardData[i].strokeWidth,
+        draggable:false,
+      });
+     
+      this.layer.add(this.line);
+      this.stage.batchDraw();
+
+        break;
+      }
+      case 'pencil':{
+      this.pen = Line.drawLine({
+      stroke: boardData[i].strokeColor,
+      strokeWidth: boardData[i].strokeWidth,
+      lineCap: 'round', 
+      lineJoin: 'round', 
+      points: boardData[i].points,
+      draggable:false,
+    });
+   
+    this.layer.add(this.pen);
+    this.stage.batchDraw();
+      }
+
+    }
+  }
+},
+
+Undo() {
+  this.requestundo()
+    .then(boardData => {
+      console.log(boardData);
+      this.draw(boardData); // Call undo() with the updated board data
+    })
+    .catch(error => {
+      // Handle errors if any from the requestundo() function
+      console.error('Undo failed:', error);
+    });
+},
+
+Redo() {
+  this.requestredo()
+    .then(boardData => {
+      console.log(boardData);
+      this.draw(boardData); // Call undo() with the updated board data
+    })
+    .catch(error => {
+      // Handle errors if any from the requestundo() function
+      console.error('Redo failed:', error);
+    });
+  },
+
+    get_element(){
+      this.stage.on('mouseover', (e) => {
+        const shape = e.target;
+        if(shape instanceof Konva.Shape){
+          shape.fill('red');
+          shape.shadowColor('black');
+          shape.shadowBlur(10);
+          shape.shadowOffset({ x: 5, y: 5 });
+          shape.shadowOpacity(0.7);
+          console.log('Hovered Shape ID:', this.shapeID);
+          console.log(shape);
+          this.layer.batchDraw();
+        }
+      });
+
+      this.stage.on('mouseout', (e) => {
+        const shape = e.target;
+        if(shape instanceof Konva.Shape){
+          shape.fill('aliceblue');
+          shape.shadowOpacity(0);
+          this.layer.batchDraw();
+        }
+      });
+    
+  },
     drawshape(shapetype)
     {
       let Shape = new ShapeDTO();
       this.stage.off('mousedown');
       this.stage.off('mousemove');
       this.stage.off('mouseup');
+      this.stage.off('mouseout')
+      this.stage.off('mouseover')
       this.stage.on('mousedown', (e) => {
       if (!this.isDrawing) {
       this.isDrawing = true;
@@ -105,7 +354,7 @@ export default {
         stroke : this.stroke_color,
         draggable:false,
       });
-      
+      this.shapeID++;
       this.layer.add(this.circle);
       this.stage.batchDraw();
       
@@ -123,16 +372,17 @@ export default {
     }
     else if (shapetype === 'rectangle')
     {
-        this.rec = rectangle.drawrectangle({
-        x: this.startx,
-        y: this.starty,
-        width: 0,
-        height: 0,
-        fill: this.fill_color,
-        strokeWidth : this.stroke_Width,
-        stroke :this.stroke_color,
-        draggable:false,
-      });
+          this.rec = rectangle.drawrectangle({
+          x: this.startx,
+          y: this.starty,
+          width: 0,
+          height: 0,
+          fill: this.fill_color,
+          strokeWidth : this.stroke_Width,
+          stroke :this.stroke_color,
+          draggable:false,
+        });
+      this.shapeID++;
       this.layer.add(this.rec);
       this.stage.batchDraw();
       this.stage.on('mousemove', (e) => {
@@ -149,8 +399,7 @@ export default {
     }
     else if (shapetype === 'square')
     {
-      let sq;
-        sq = Square.drawrsquare({
+        this.sq = Square.drawsquare({
         x: this.startx,
         y: this.starty,
         width: 0,
@@ -160,7 +409,8 @@ export default {
         stroke :this.stroke_color,
         draggable:false,
       });
-      this.layer.add(sq);
+      this.shapeID++;
+      this.layer.add(this.sq);
       this.stage.batchDraw();
 
       this.stage.on('mousemove', (e) => {
@@ -170,8 +420,8 @@ export default {
           const side = Math.min(Math.abs(width), Math.abs(height));
 
          
-          sq.width(side * Math.sign(width));
-          sq.height(side * Math.sign(height));
+          this.sq.width(side * Math.sign(width));
+          this.sq.height(side * Math.sign(height));
 
           this.stage.batchDraw();
         }
@@ -179,8 +429,7 @@ export default {
     }
     else if(shapetype==='triangle')
     {
-      let triangle;
-      triangle = Triangle.drawTriangle({
+        this.triangle = Triangle.drawTriangle({
         x: this.startx,
         y: this.starty,
         sides: 3,
@@ -190,8 +439,8 @@ export default {
         stroke :this.stroke_color,
         draggable:false,
       });
-
-      this.layer.add(triangle);
+      this.shapeID++;
+      this.layer.add(this.triangle);
       this.stage.batchDraw();
 
       this.stage.on('mousemove', (e) => {
@@ -202,7 +451,7 @@ export default {
           );
 
           // Set the radius of the triangle to determine its size
-          triangle.radius(side);
+          this.triangle.radius(side);
           this.stage.batchDraw();
         }
       });
@@ -211,8 +460,7 @@ export default {
 
     else if (shapetype==='ellipse')
     {
-      let ellip;
-      ellip = ellipse.drawellipse({
+      this.ellip = ellipse.drawellipse({
         x: this.startx,
         y: this.starty,
         radiusX: 0,
@@ -222,8 +470,8 @@ export default {
         stroke :this.stroke_color,
         draggable:false,
       });
-
-      this.layer.add(ellip);
+      this.shapeID++;
+      this.layer.add(this.ellip);
       this.stage.batchDraw();
 
       this.stage.on('mousemove', (e) => {
@@ -231,8 +479,8 @@ export default {
           const radiusX = Math.abs(e.evt.offsetX - this.startx);
           const radiusY = Math.abs(e.evt.offsetY - this.starty);
 
-          ellip.radiusX(radiusX);
-          ellip.radiusY(radiusY);
+          this.ellip.radiusX(radiusX);
+          this.ellip.radiusY(radiusY);
           this.stage.batchDraw();
         }
       });
@@ -240,22 +488,21 @@ export default {
     }
     else if (shapetype==='line')
     {
-      let line;
-      line = Line.drawLine({
+        this.line = Line.drawLine({
         points: [this.startx, this.starty],
         stroke: this.stroke_color,
         strokeWidth: this.stroke_Width,
         draggable:false,
       });
-
-      this.layer.add(line);
+      this.shapeID++;
+      this.layer.add(this.line);
       this.stage.batchDraw();
 
       this.stage.on('mousemove', (e) => {
         if (this.isDrawing) {
           const endx = e.evt.offsetX;
           const endy = e.evt.offsetY;
-          line.points([this.startx, this.starty, endx, endy]);
+          this.line.points([this.startx, this.starty, endx, endy]);
           this.stage.batchDraw();
         }
       });
@@ -264,7 +511,7 @@ export default {
     else if (shapetype==='pencil')
     {
       let pos = this.stage.getPointerPosition();
-      let lastLine = Line.drawLine({
+      this.pen = Line.drawLine({
       stroke: this.stroke_color,
       strokeWidth: this.stroke_Width,
       lineCap: 'round', 
@@ -272,7 +519,9 @@ export default {
       points: [pos.x, pos.y],
       draggable:false,
     });
-    this.layer.add(lastLine);
+    this.shapeID++;
+    this.layer.add(this.pen);
+    this.stage.batchDraw();
   
 
   this.stage.on('mousemove touchmove', () => {
@@ -280,8 +529,8 @@ export default {
       return;
     }
     let pos = this.stage.getPointerPosition();
-    let newPoints = lastLine.points().concat([pos.x, pos.y]);
-    lastLine.points(newPoints);
+    let newPoints = this.pen.points().concat([pos.x, pos.y]);
+    this.pen.points(newPoints);
     this.layer.batchDraw();
   });
     }
@@ -293,6 +542,8 @@ export default {
         }
         if (shapetype === 'circle')
         {
+          if(this.circle.radius()==0)
+            return;
           Shape.shapeType = 'circle';
           Shape.radius = this.circle.radius();
           Shape.x = this.circle.x();
@@ -300,19 +551,96 @@ export default {
           Shape.fill = this.circle.fill();
           Shape.strokeColor = this.circle.stroke();
           Shape.strokeWidth = this.circle.strokeWidth();
-
+          Shape.id = this.shapeID;
+    
         }
         if (shapetype === 'rectangle')
         {
+          if (this.rec.height()===0 || this.rec.width()===0)
+            return;
           Shape.shapeType = 'rectangle';
           Shape.x = this.rec.x();
           Shape.y = this.rec.y();
+          Shape.width = this.rec.width();
+          Shape.height = this.rec.height();
           Shape.fill = this.rec.fill();
           Shape.strokeColor = this.rec.stroke();
           Shape.strokeWidth = this.rec.strokeWidth();
+          Shape.id = this.shapeID;
+
 
         }
-        console.log(Shape);
+        if (shapetype === 'square')
+        {
+          if (this.sq.height()===0 || this.sq.width()===0)
+            return;
+          Shape.shapeType = 'rectangle';
+          Shape.x = this.sq.x();
+          Shape.y = this.sq.y();
+          Shape.width = this.sq.width();
+          Shape.height = this.sq.height();
+          Shape.fill = this.sq.fill();
+          Shape.strokeColor = this.sq.stroke();
+          Shape.strokeWidth = this.sq.strokeWidth();
+          Shape.id = this.shapeID;
+        }
+        if (shapetype === 'triangle')
+        {
+          if (this.triangle.radius()==0)
+            return;
+          Shape.shapeType = 'triangle';
+          Shape.x = this.triangle.x();
+          Shape.y = this.triangle.y();
+          Shape.radius = this.triangle.radius();
+          Shape.sides = this.triangle.sides()
+          Shape.fill = this.triangle.fill();
+          Shape.strokeColor = this.triangle.stroke();
+          Shape.strokeWidth = this.triangle.strokeWidth();
+          Shape.id = this.shapeID;
+        }
+        if (shapetype === 'ellipse')
+        {
+          if(this.ellip.radiusX()==0 || this.ellip.radiusY()==0)
+            return;
+          Shape.shapeType = 'ellipse';
+          Shape.radiusX = this.ellip.radiusX();
+          Shape.radiusY = this.ellip.radiusY();
+          Shape.x = this.ellip.x();
+          Shape.y = this.ellip.y();
+          Shape.fill = this.ellip.fill();
+          Shape.strokeColor = this.ellip.stroke();
+          Shape.strokeWidth = this.ellip.strokeWidth();
+          Shape.id = this.shapeID;
+    
+        }
+        if (shapetype === 'line')
+        {
+          if(this.line.points().length<4)
+            return;
+          Shape.shapeType = 'line_segment';
+          Shape.points = this.line.points();
+          Shape.fill = this.line.fill();
+          Shape.strokeColor = this.line.stroke();
+          Shape.strokeWidth = this.line.strokeWidth();
+          Shape.id = this.shapeID;
+    
+        }
+        if (shapetype === 'pencil')
+        {
+          if(this.pen.points().length<=2)
+            return;
+          Shape.shapeType = 'pencil';
+          Shape.points = this.pen.points();
+          Shape.fill = this.pen.fill();
+          Shape.strokeColor = this.pen.stroke();
+          Shape.strokeWidth = this.pen.strokeWidth();
+          Shape.id = this.shapeID;
+    
+        }
+        
+
+
+        this.requestdraw(Shape);
         this.stage.off('mouseup');
 
       });
@@ -426,6 +754,26 @@ changeFillColor(event) {
     list-style: none;
   
   }
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content{
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 40%;
+  text-align: center;
+}
   button
   {
     box-shadow: none;
--- front-end/ShapeDTO.js
+++ front-end/ShapeDTO.js
@@ -1,4 +1,5 @@
 class ShapeDTO {
+    points = [];
     id = null;
     x = null;
     y = null;
@@ -10,8 +11,11 @@ class ShapeDTO {
     shapeType = null;
     radiusX = null;
     radiusY = null;
+    scaleX = null;
+    scaleY = null;
     width = null;
     height = null;
+    sides = null;
 
 }
 export default ShapeDTO;
